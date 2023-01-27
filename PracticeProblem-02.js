923d71b83fc9026e27579e2b3ca91d79162040b4
senior9 <ovi215188@gmail.com>
2023-01-28 02:59:40 +0600

    practice problem two

---
+++ PracticeProblem-02.js
@@ -0,0 +1,34 @@
+var gradeA=80;
+var gradeB=60;
+var gradeC=50;
+var gradeD=40;
+var gradeF=39;
+var myResult =85;
+var tomResult = 66;
+var janeResult = 95;
+var peterResult =56 ;
+var johnResult = 40 ;
+ 
+if (peterResult >= gradeA ) {
+     console.log('A+ paice')
+}
+else if (myResult,tomResult,janeResult,peterResult,janeResult >= gradeB,gradeC,gradeD,gradeF ) {
+    console.log('Pass Korche ')
+}
+ var num1= 13;
+ var num2=79;
+ var num3=45;
+
+ if (num1>num2 && num1>num3) {
+    console.log('The largest Number is : ',num1);
+ }
+ else if(num2>num3 && num2> num1){
+    console.log('The largest Number is : ',num2)
+ }
+ else if(num3>num1 && num3>num2){
+    console.log('The largest Number is : ',num3)
+ }
+
+
+
+
